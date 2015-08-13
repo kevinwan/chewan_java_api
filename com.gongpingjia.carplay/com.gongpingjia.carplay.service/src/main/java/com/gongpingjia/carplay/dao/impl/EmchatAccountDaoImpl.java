4ef08b0c6126d037062deea7079da252ac3b305b
GPxiaolongwang <lxwang@gongpingjia.com>
2015-08-13 17:04:08 +0800

    add EmchatAccountDaoImpl

---
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/dao/impl/EmchatAccountDaoImpl.java
@@ -0,0 +1,35 @@
+package com.gongpingjia.carplay.dao.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.gongpingjia.carplay.dao.EmchatAccountDao;
+import com.gongpingjia.carplay.po.EmchatAccount;
+
+@Service
+public class EmchatAccountDaoImpl implements EmchatAccountDao {
+
+	@Override
+	public int deleteByPrimaryKey(String userid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(EmchatAccount record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public EmchatAccount selectByPrimaryKey(String userid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateByPrimaryKey(EmchatAccount record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
