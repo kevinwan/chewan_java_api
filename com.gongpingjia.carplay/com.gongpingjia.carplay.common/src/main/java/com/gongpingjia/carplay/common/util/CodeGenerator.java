50cba3e92796824fb6af83d0d636e24e9c8a0eb5
licheng <chengli@gongpingjia.com>
2015-08-13 14:34:25 +0800

    Modify CodeGenerator Pattern

--- com.gongpingjia.carplay/com.gongpingjia.carplay.common/src/main/java/com/gongpingjia/carplay/common/util/CodeGenerator.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.common/src/main/java/com/gongpingjia/carplay/common/util/CodeGenerator.java
@@ -1,6 +1,7 @@
 package com.gongpingjia.carplay.common.util;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 import org.springframework.util.StringUtils;
 
@@ -12,7 +13,11 @@ import org.springframework.util.StringUtils;
  */
 public class CodeGenerator {
 
-	private static final String UUID_REGIX = "^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$";
+	/**
+	 * UUID的正则表达式匹配
+	 */
+	private static final Pattern UUID_PATTERN = Pattern.compile(
+			"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$", Pattern.CASE_INSENSITIVE);
 
 	/**
 	 * 生成数据表的主键，采用UUID生成
@@ -45,6 +50,6 @@ public class CodeGenerator {
 		if (StringUtils.isEmpty(uuid)) {
 			return false;
 		}
-		return uuid.matches(UUID_REGIX);
+		return UUID_PATTERN.matcher(uuid).matches();
 	}
 }
