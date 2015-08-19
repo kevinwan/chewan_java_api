3918db35b38f428db5c0e92dea0faa01ca6dcf86
GPxiaolongwang <lxwang@gongpingjia.com>
2015-08-19 15:17:36 +0800

    user

--- com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/test/java/com/gongpingjia/carplay/controller/server/UserTest.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/test/java/com/gongpingjia/carplay/controller/server/UserTest.java
@@ -12,22 +12,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class UserTest extends BaseTest {
 
 	@Test
-	public void testloginUser() throws Exception {
-		MvcResult result = mockMvc
-				.perform(
-						MockMvcRequestBuilders.post("/user/login")
-								.param("phone", "18951650021")
-								.param("password", "e10adc3949ba59abbe56e057f20f883e"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().encoding("UTF-8"))
-				.andExpect(
-						MockMvcResultMatchers.content().contentType(
-								"application/json;charset=UTF-8"))
-				.andDo(MockMvcResultHandlers.print()).andReturn();
-		Assert.assertNull(result.getModelAndView());
-	}
-	
-	@Test
 	public void testForgetPassword() throws Exception {
 		MvcResult result = mockMvc
 				.perform(
