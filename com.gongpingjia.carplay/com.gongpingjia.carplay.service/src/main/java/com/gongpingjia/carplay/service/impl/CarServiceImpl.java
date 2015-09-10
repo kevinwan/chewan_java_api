6cc6810062a68eb7e9e235e17930bc8b521e8386
shuofu <shuofu@gongpingjia.com>
2015-09-10 11:56:42 +0800

    fix bug

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/service/impl/CarServiceImpl.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/service/impl/CarServiceImpl.java
@@ -37,13 +37,13 @@ public class CarServiceImpl implements CarService {
 	public ResponseDo getCarBrand() throws ApiException {
 
 		LOG.debug("Query data from cache first");
-//		JSONObject jsonCache = cacheManager.getCarBrand();
-//		if (jsonCache != null) {
-//			return ResponseDo.buildSuccessResponse(jsonCache);
-//		}
+		JSONObject jsonCache = cacheManager.getCarBrand();
+		if (jsonCache != null) {
+			return ResponseDo.buildSuccessResponse(jsonCache);
+		}
 
 		LOG.debug("Query data from gongpingjia if no data exist in the cache");
-		String data = new String();
+		JSONObject json = new JSONObject();
 		String gpjUrl = PropertiesUtil.getProperty("gongpingjia.brand.url", "");
 
 		Header header = new BasicHeader("Accept", "application/json");
@@ -54,16 +54,16 @@ public class CarServiceImpl implements CarService {
 			response = HttpClientUtil.get(gpjUrl, new HashMap<String, String>(0), Arrays.asList(header),
 					Constants.Charset.UTF8);
 
-			String dataGpj = HttpClientUtil.parseResponse(response);
-			data = JSONObject.fromObject(dataGpj).getJSONArray("brand").toString();
-
+			String data = HttpClientUtil.parseResponse(response);
+			json = JSONObject.fromObject(data);
+			json.put("data", json.getJSONArray("brand"));
 			LOG.debug("Refresh brand info in cache server");
 			cacheManager.setCarBrand(data);
 		} finally {
 			HttpClientUtil.close(response);
 		}
 
-		return ResponseDo.buildSuccessResponse(data);
+		return ResponseDo.buildSuccessResponse(json);
 	}
 
 	/**
