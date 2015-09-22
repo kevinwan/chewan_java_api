0eaeaaaef8527d918d7061d3cd5a9cfe05a1d287
heyongyu <oncestarts@gmail.com>
2015-09-22 18:17:21 +0800

    ActivityController

---
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/test/java/com/gongpingjia/carplay/controller/business/ActivityTest.java
@@ -0,0 +1,17 @@
+package com.gongpingjia.carplay.controller.business;
+
+import com.gongpingjia.carplay.controller.BaseTest;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * Created by Administrator on 2015/9/22.
+ */
+public class ActivityTest extends BaseTest {
+
+    public void testActivityRegister(){
+//        mockMvc
+//				.perform(MockMvcRequestBuilders.post("/phone/" + phone + "/verification").param("code", phoneVerify.getCode()))
+//				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+    }
+}
