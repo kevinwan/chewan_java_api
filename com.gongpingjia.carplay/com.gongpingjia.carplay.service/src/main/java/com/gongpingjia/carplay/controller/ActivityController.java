c1bac5b365def6187a3469f5fc6b8ba740ef9aab
heyongyu <oncestarts@gmail.com>
2015-09-23 09:28:21 +0800

    ActivityController

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/ActivityController.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/ActivityController.java
@@ -24,18 +24,20 @@ public class ActivityController {
 
     @RequestMapping(value = "/activity/register", method = RequestMethod.POST, headers = {
             "Accept=application/json; charset=UTF-8", "Content-Type=application/json"})
+    @ResponseBody
     public ResponseDo registerActivity(@RequestParam(value = "userId") String userId, @RequestParam(value = "token") String token, @Validated @RequestParam Activity activity) {
         LOG.debug("activity/register begin");
         try {
-         return    activityService.activityRegister(userId, token, activity);
+            return activityService.activityRegister(userId, token, activity);
         } catch (Exception e) {
             LOG.warn(e.getMessage(), e);
             return ResponseDo.buildFailureResponse(e.getMessage());
         }
     }
 
-    @RequestMapping(value = "/activity/${activityId}/info",method = RequestMethod.GET)
-    public ResponseDo getActivityInfo(@PathVariable("activityId")String activityId,@RequestParam("userId")String userId,@RequestParam("token") String token){
+    @RequestMapping(value = "/activity/${activityId}/info", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseDo getActivityInfo(@PathVariable("activityId") String activityId, @RequestParam("userId") String userId, @RequestParam("token") String token) {
         LOG.debug("activity/{activityId}/info begin");
         try {
             return activityService.getActivityInfo(userId, token, activityId);
