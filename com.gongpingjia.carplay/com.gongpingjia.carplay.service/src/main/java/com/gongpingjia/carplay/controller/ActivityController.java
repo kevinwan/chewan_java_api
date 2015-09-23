584969689eaa7bf15bbaf2928f4981187d5233b2
heyongyu <oncestarts@gmail.com>
2015-09-23 17:20:17 +0800

    ActivityController

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/ActivityController.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/ActivityController.java
@@ -1,15 +1,23 @@
 package com.gongpingjia.carplay.controller;
 
 import com.gongpingjia.carplay.common.domain.ResponseDo;
+import com.gongpingjia.carplay.common.exception.ApiException;
 import com.gongpingjia.carplay.entity.activity.Activity;
+import com.gongpingjia.carplay.entity.common.Landmark;
 import com.gongpingjia.carplay.service.ActivityService;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by heyongyu on 2015/9/22.
@@ -68,4 +76,31 @@ public class ActivityController {
             return ResponseDo.buildFailureResponse(e.getMessage());
         }
     }
+
+
+    @RequestMapping(value = "/activity/list",method = RequestMethod.GET)
+    public ResponseDo getNearByActivityList(HttpServletRequest request,@RequestParam("userId")String userId,@RequestParam("token")String token) {
+        LOG.debug("activity/{activityId}/info begin");
+        try {
+            return activityService.getNearActivityList(initTransParam(request),request);
+        } catch (Exception e) {
+            LOG.warn(e.getMessage(), e);
+            return ResponseDo.buildFailureResponse(e.getMessage());
+        }
+    }
+
+    /**
+     * type=$type&pay=$pay&province=$province&city=$city&district=$district&street=$street&transfer=$transfer&ignore=$ignore&limit=$limit&longitude=$longitude&latitude=$latitude &maxDistance=$maxDistance
+     */
+    private Map<String, String> initTransParam(HttpServletRequest request) {
+        Map<String, String> keyTranMap = new HashMap<>();
+        keyTranMap.put("pay","pay");
+        keyTranMap.put("type","type");
+        keyTranMap.put("province", "establish.province");
+        keyTranMap.put("city", "establish.city");
+        keyTranMap.put("district", "establish.district");
+        keyTranMap.put("street", "establish.street");
+        return keyTranMap;
+    }
+
 }
--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/service/ActivityService.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/service/ActivityService.java
@@ -4,6 +4,10 @@ package com.gongpingjia.carplay.service;
 import com.gongpingjia.carplay.common.domain.ResponseDo;
 import com.gongpingjia.carplay.common.exception.ApiException;
 import com.gongpingjia.carplay.entity.activity.Activity;
+import org.springframework.data.mongodb.core.query.Query;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Created by heyongyu on 2015/9/22.
@@ -21,4 +25,9 @@ public interface ActivityService {
      */
     public ResponseDo getActivityInfo(String userId, String token, String activityId) throws ApiException;
 
+
+    public ResponseDo getNearActivityList(Map<String, String> transParams, HttpServletRequest request) throws ApiException;
+
+
+    public Query initQuery(HttpServletRequest request, Map<String, String> transMap);
 }
