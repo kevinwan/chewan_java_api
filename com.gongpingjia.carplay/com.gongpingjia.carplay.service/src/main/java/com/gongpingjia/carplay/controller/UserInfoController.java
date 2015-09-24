e46a0bfa257bc5323fbdafb86cca15f6b979815c
shuofu <shuofu@gongpingjia.com>
2015-09-24 19:09:37 +0800

    commit for add controller/UserInfoController.java

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/UserInfoController.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/UserInfoController.java
@@ -110,12 +110,6 @@ public class UserInfoController {
     /**
      * 三方登录
      *
-     * @param uid      三方登录返回的用户唯一标识
-     * @param channel  wechat 、qq 或 sinaWeibo
-     * @param sign     API签名，计算方法为 MD5(uid + channel + BundleID) 其中，BundleID 为
-     *                 com.gongpingjia.carplay
-     * @param username 三方登录返回的用户昵称
-     * @param url      三方登录返回的用户头像地址
      * @return 返回登录结果
      */
     @RequestMapping(value = "/sns/login", method = RequestMethod.POST, headers = {
