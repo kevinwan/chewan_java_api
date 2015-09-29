6f6168ce05c0efe8c61dd5effe2614a057416457
licheng <chengli@gongpingjia.com>
2015-09-29 20:57:13 +0800

    Commit by adjust codes

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/UserInfoController.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/controller/UserInfoController.java
@@ -36,7 +36,7 @@ public class UserInfoController {
         LOG.debug("register is called, request parameter produce:");
         try {
             // 检查必须参数是否为空
-            if (CommonUtil.isEmpty(json, Arrays.asList("nickname", "gender", "birthday", "avatar"))) {
+            if (CommonUtil.isEmpty(json, Arrays.asList("nickname", "gender", "birthday", "avatar", "landmark"))) {
                 throw new ApiException("输入参数错误");
             }
 
