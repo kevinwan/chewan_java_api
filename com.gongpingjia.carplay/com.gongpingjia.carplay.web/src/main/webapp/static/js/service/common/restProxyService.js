487006bdae44afba41df658e12ea13e84fae11ae
licheng <chengli@gongpingjia.com>
2015-10-09 17:47:32 +0800

    Commit by adjust post method

--- com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/main/webapp/static/js/service/common/restProxyService.js
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/main/webapp/static/js/service/common/restProxyService.js
@@ -2,25 +2,26 @@
 
 /**
  * AuthController
- * 
+ *
  * @constructor
  */
-	
-gpjApp.factory('restProxyService', ['$http', function($http) {
+
+gpjApp.factory('restProxyService', ['$http', function ($http) {
 
     return {
-		/**
-		 * Send http get request
-		 */
-		sendHttpGet : function(apiPrefix, path) {
-		    return $http.get(apiPrefix + encodeURI(path));
-		},
-	
-		/**
-		 * Send http post request
-		 */
-		sendHttpPost : function(apiPrefix, path, data) {
-			 return $http.post(apiPrefix + path, data);
-		}
+        /**
+         * Send http get request
+         */
+        sendHttpGet: function (apiPrefix, path) {
+            return $http.get(apiPrefix + encodeURI(path));
+        },
+
+        /**
+         * Send http post request
+         */
+        sendHttpPost: function (apiPrefix, path, data) {
+            $http.headers = {"Accept": "application/json; charset=UTF-8", "Content-Type": "application/json"};
+            return $http.post(apiPrefix + path, data);
+        }
     }
-} ]);
+}]);
