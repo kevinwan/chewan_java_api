b54ff89c346d4f64c17d4db0f74c3f10bbb622d8
licheng <chengli@gongpingjia.com>
2015-10-27 13:38:58 +0800

    Commit by try to add statistic

---
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.data/src/main/java/com/gongpingjia/carplay/dao/statistic/ActivityStatisticDao.java
@@ -0,0 +1,10 @@
+package com.gongpingjia.carplay.dao.statistic;
+
+import com.gongpingjia.carplay.dao.common.BaseDao;
+import com.gongpingjia.carplay.entity.statistic.ActivityStatistic;
+
+/**
+ * Created by licheng on 2015/10/27.
+ */
+public interface ActivityStatisticDao extends BaseDao<ActivityStatistic, String> {
+}
