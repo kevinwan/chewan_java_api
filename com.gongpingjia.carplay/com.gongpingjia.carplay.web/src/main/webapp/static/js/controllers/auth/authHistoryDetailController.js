3be2b7f3495e8d22980b76ff1c1c8b89789313bb
oncestarts@gmail.com <oncestarts@gmail.com>
2015-11-03 20:25:00 +0800

    样式调整

--- com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/main/webapp/static/js/controllers/auth/authHistoryDetailController.js
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.web/src/main/webapp/static/js/controllers/auth/authHistoryDetailController.js
@@ -8,9 +8,13 @@
  *
  * @constructor
  */
-gpjApp.controller('authHistoryDetailController', ['$scope', '$rootScope', '$window', 'authenticationService',
-    function ($scope, $rootScope, $window, authenticationService) {
+gpjApp.controller('authHistoryDetailController', ['$scope', '$rootScope', '$window', 'authenticationService','$modalInstance',
+    function ($scope, $rootScope, $window, authenticationService,$modalInstance) {
         $rootScope.loadingPromise = authenticationService.getAuthHistorys().success(function(result){
             $scope.authHistorys = result.data;
         });
+
+        $scope.close = function(){
+            $modalInstance.dismiss('close');
+        }
     }]);
