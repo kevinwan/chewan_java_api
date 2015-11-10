d781ca28e600eb0f1f04a1de8cb8d193479800db
oncestarts <oncestarts@gmail.com>
2015-11-10 10:50:54 +0800

    定位ImageIo问题

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/initial/ContextConfiguration.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/initial/ContextConfiguration.java
@@ -1,5 +1,8 @@
 package com.gongpingjia.carplay.initial;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -8,18 +11,23 @@ import sun.awt.AppContext;
 import sun.awt.SunToolkit;
 
 /**
-* Created by Administrator on 2015/11/9.
-*/
+ * Created by Administrator on 2015/11/9.
+ */
 @SuppressWarnings("restriction")
 @Service
 @Scope("singleton")
 public class ContextConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
-    @Override
+
+    private static Logger logger = LoggerFactory.getLogger(ContextConfiguration.class);
+
     public void onApplicationEvent(ContextRefreshedEvent arg0) {
         //refresh awt context
         if (AppContext.getAppContext() == null) {
+            logger.info("^^^^^^^^^^^^^^^^^^^____^^^^^^^^^^^^^^^^^^^^^  AppContext.getAppContext is null ^^^^^^^^^^^^^^^^^^^____^^^^^^^^^^^^^^^^^^^^^");
             SunToolkit.createNewAppContext();
+        } else {
+            logger.info("^^^^^^^^^^^^^^^^^^^____^^^^^^^^^^^^^^^^^^^^^  AppContext.getAppContext not null ^^^^^^^^^^^^^^^^^^^____^^^^^^^^^^^^^^^^^^^^^");
         }
     }
 }
