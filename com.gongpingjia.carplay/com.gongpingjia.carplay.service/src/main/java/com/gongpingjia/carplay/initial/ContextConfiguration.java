26ddbedd74ba3979b9db0cefff92d68c57453325
oncestarts <oncestarts@gmail.com>
2015-11-10 10:45:59 +0800

    ImageIo jdk bug fix

--- com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/initial/ContextConfiguration.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.service/src/main/java/com/gongpingjia/carplay/initial/ContextConfiguration.java
@@ -1,25 +1,25 @@
-//package com.gongpingjia.carplay.initial;
-//
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.context.annotation.Scope;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.stereotype.Service;
-//import sun.awt.AppContext;
-//import sun.awt.SunToolkit;
-//
-///**
-// * Created by Administrator on 2015/11/9.
-// */
-//@SuppressWarnings("restriction")
-//@Service
-//@Scope("singleton")
-//public class ContextConfiguration implements ApplicationListener<ContextRefreshedEvent> {
-//
-//    @Override
-//    public void onApplicationEvent(ContextRefreshedEvent arg0) {
-//        //refresh awt context
-//        if (AppContext.getAppContext() == null) {
-//            SunToolkit.createNewAppContext();
-//        }
-//    }
-//}
+package com.gongpingjia.carplay.initial;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+import sun.awt.AppContext;
+import sun.awt.SunToolkit;
+
+/**
+* Created by Administrator on 2015/11/9.
+*/
+@SuppressWarnings("restriction")
+@Service
+@Scope("singleton")
+public class ContextConfiguration implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+        //refresh awt context
+        if (AppContext.getAppContext() == null) {
+            SunToolkit.createNewAppContext();
+        }
+    }
+}
