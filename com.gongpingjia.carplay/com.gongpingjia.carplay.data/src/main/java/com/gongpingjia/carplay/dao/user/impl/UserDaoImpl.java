72d2ad12328e1449e7de0b3ae2b17a945630704b
oncestarts <oncestarts@gmail.com>
2015-11-18 10:26:14 +0800

    活动添加页面遗漏

--- com.gongpingjia.carplay/com.gongpingjia.carplay.data/src/main/java/com/gongpingjia/carplay/dao/user/impl/UserDaoImpl.java
+++ com.gongpingjia.carplay/com.gongpingjia.carplay.data/src/main/java/com/gongpingjia/carplay/dao/user/impl/UserDaoImpl.java
@@ -1,8 +1,16 @@
 package com.gongpingjia.carplay.dao.user.impl;
 
+import com.gongpingjia.carplay.common.util.CommonUtil;
+import com.gongpingjia.carplay.common.util.Constants;
+import com.gongpingjia.carplay.dao.common.PhotoDao;
 import com.gongpingjia.carplay.dao.common.impl.BaseDaoImpl;
 import com.gongpingjia.carplay.dao.user.UserDao;
+import com.gongpingjia.carplay.entity.common.Photo;
 import com.gongpingjia.carplay.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 /**
